06aad39bc021a7fce114a1f17fdb4de81dc5f26d
Brian O'Grady <brian.ogrady@justplainlogic.com>
2017-08-07 13:19:02 -0700

    Moved content to .exs file.

--- secret_handshake/secret_handshake.exs
+++ secret_handshake/secret_handshake.exs
@@ -1,4 +1,5 @@
 defmodule SecretHandshake do
+  use Bitwise, only_operators: true
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -13,31 +14,44 @@ defmodule SecretHandshake do
 
   10000 = Reverse the order of the operations in the secret handshake
   """
+  @codes [wink: 0b1, double_blink: 0b10, close_your_eyes: 0b100, jump: 0b1000, reverse: 0b10000]
+
   @spec commands(code :: integer) :: list(String.t())
-
   def commands(code) do
-    digitToBin(code)
-  end
-
-  @spec digitToBin(integer) :: [integer]
-  def digitToBin(num) do
-    Integer.digits(num, 2)
-    |> Enum.reverse
-    |> Enum.reduce(fn num, acc -> num * Math.power(10, acc) end)
+    decode(code)
+    |> reverse_it
+    |> to_s
   end
   
-  def printTheMagic(binlist) when length >= 5, do: Enum.reverse(binlist) |> printTheMagic
-    def printTheMagic(binlist) do
-      case Enum.map(binlist, fn index -> index.binlist end) do
-        1 ->
-          "wink"
-        10 ->
-          "double blink"
-        100 ->
-          "close your eyes"
-        1000 ->
-          "jump"
-      end
-    end 
+  @spec decode(code :: integer) :: list({atom, integer})
+  defp decode(code) do
+    Enum.filter(@codes, fn 
+      {event, flag} ->
+        (code &&& flag) == flag
+    end)
+  end
+
+  @spec reverse_it(decoded_list :: list({atom, integer})) :: list({atom, integer})
+  defp reverse_it(decoded_list) do
+    case Keyword.has_key?(decoded_list, :reverse) do
+      true ->
+        [_ | tail] = Enum.reverse(decoded_list)
+        tail
+      false -> 
+        decoded_list
+    end
+  end 
+
+  @spec to_s(pattern :: list({atom, integer})) :: list(String.t())
+  defp to_s(pattern) do
+    Keyword.keys(pattern)
+    |> Enum.map(fn code -> Atom.to_string(code) end)
+    |> remove_that_sp
+  end
+
+  @spec remove_that_sp(stringlist :: list(String.t())) :: list(String.t())
+  defp remove_that_sp(stringlist) do
+    Enum.map(stringlist, fn code -> String.replace(code, "_", " ") end)
+  end
 end
 
